546bbf677f9df9ce0135a9d1c9ba35eaa11a8dce
Piotr Szczepanski <piotr.szczepanski@avg.com>
2016-01-19 14:39:06 +0100

    Typo

--- ida2latex.cpp
+++ ida2latex.cpp
@@ -259,15 +259,15 @@ char help[] =
 
 //--------------------------------------------------------------------------
 // This is the preferred name of the plugin module in the menu system
-// The preferred name may be overriden in plugins.cfg file
+// The preferred name may be overridden in plugins.cfg file
 
 char wanted_name[] = "IDA2LaTeX";
 
 
 // This is the preferred hotkey for the plugin module
-// The preferred hotkey may be overriden in plugins.cfg file
+// The preferred hotkey may be overridden in plugins.cfg file
 // Note: IDA won't tell you if the hotkey is not correct
-//       It will just disable the hotkey.
+// It will just disable the hotkey.
 
 char wanted_hotkey[] = "Ctrl-Alt-9";
 
